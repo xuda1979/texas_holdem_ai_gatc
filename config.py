d4b2ebe6ac1a43a2cb70926f83d7817879d1c282
David Hsu <exutechnology@gmail.com>
2025-09-12 13:29:40 +0800

    Fix pre-commit lint issues

--- config.py
+++ config.py
@@ -1,1 +1,5 @@
-from poker_ai.config.config import *
+"""Re-export configuration constants without wildcard import."""
+
+from poker_ai.config.config import BASE_DATA_DIR, MODEL_DIR, SIMULATED_DATA_DIR
+
+__all__ = ["BASE_DATA_DIR", "MODEL_DIR", "SIMULATED_DATA_DIR"]
