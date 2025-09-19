8c85b76a9e0c3315c349641d21703d02fc51c616
David Hsu <exutechnology@gmail.com>
2025-09-20 00:53:40 +0800

    Add periodic model saving to simple training loop (#156)

--- training/train.py
+++ training/train.py
@@ -11,6 +11,15 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument("--num-hands", type=int, default=100, help="Number of hands to simulate")
     parser.add_argument("--num-players", type=int, default=2, help="Number of players at the table")
     parser.add_argument("--starting-stack", type=int, default=1000, help="Starting chip stack")
+    parser.add_argument(
+        "--save-interval",
+        type=int,
+        default=0,
+        help=(
+            "Number of hands between intermediate model saves. "
+            "A value of 0 disables periodic saving."
+        ),
+    )
     return parser.parse_args()
 
 
@@ -24,8 +33,10 @@ def main() -> None:
     trainer = AICFRTrainer()
     self_play_env = SelfPlay(cfr_trainer=trainer, game_engine_config=game_cfg)
 
-    for _ in range(args.num_hands):
+    for hand_idx in range(1, args.num_hands + 1):
         self_play_env.play_hand_for_training()
+        if args.save_interval and hand_idx % args.save_interval == 0:
+            trainer.save_model()
     trainer.save_model()
 
 
